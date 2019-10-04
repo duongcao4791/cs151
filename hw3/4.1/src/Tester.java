7322da44342dd8408750844927757137ca1ccc9c
Duong Cao <duong@Duongs-MacBook-Pro.local>
2019-10-04 16:40:30 -0700

    add doc

--- hw3/4.1/src/Tester.java
+++ hw3/4.1/src/Tester.java
@@ -4,17 +4,23 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Tester {
    public static void main(String[] args){
+      //Create the Jframe
       JFrame frame = new JFrame();
       frame.setPreferredSize(new Dimension(500,500));
       frame.setLayout(new GridLayout(2,1));
+
+      //Create Actomic variable width for changing the size
       AtomicInteger width = new AtomicInteger(100);
-      JPanel panel = new JPanel();
+
+      //Create car label
       CarIcon car = new CarIcon(100);
       JLabel label = new JLabel(car);
 
+      //Create JButton for Zoom in and Zoom out
       JButton in = new JButton("Zoom in");
       JButton out = new JButton(("Zoom out"));
 
+      //AddActionListener
       in.addActionListener(e -> {
          car.resize(width.addAndGet(-30));
          label.repaint();
@@ -24,6 +30,8 @@ public class Tester {
          label.repaint();
       });
 
+      //Create JPanel
+      JPanel panel = new JPanel();
       panel.add(in);
       panel.add(out);
 
