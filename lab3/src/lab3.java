712ee19c1c89ec64f54853ad1426d849011ed217
Duong Cao <duong@Duongs-MacBook-Pro.local>
2019-10-28 14:10:12 -0700

    fix

--- lab3/src/lab3.java
+++ lab3/src/lab3.java
@@ -3,14 +3,14 @@ import java.util.concurrent.RecursiveAction;
 
 public class lab3 {
     public static void dumpArray(Object[] obj) {
-        for (int i = 0; i < obj.length; i++) {
-            System.out.println(obj[i].toString());
+        for (Object arr : obj) {
+            System.out.println(arr.toString());
         }
 
     }
 
     public static void main(String[] args){
-        Object[] obj = {("text"), 8, new Rectangle(50,80)};
+        Object[] obj = {"text", 8, new Rectangle(50,80),"Hello", new };
         dumpArray(obj);
     }
 }
