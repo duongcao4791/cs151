c657c225979976712cb94037da4def3cc430b81f
Duong Cao <duong@Duongs-MacBook-Pro.local>
2019-10-28 22:00:04 -0700

    fix

--- lab3/src/lab3.java
+++ lab3/src/lab3.java
@@ -5,10 +5,19 @@ import java.util.Arrays;
 public class lab3 {
 
     public static void dumpArray(Object[] obj) {
-        System.out.println(Arrays.deepToString(obj));
+        for(int i=1; i<obj.length;i++)
+        {
+            if (obj[i] instanceof Object[] || obj[i] == null) {
+                Object[] s = (Object[]) obj[i];
+                System.out.println(Arrays.deepToString(s));
+            }
+            else
+                System.out.println(obj[i].toString());
+        }
     }
     public static void main(String[] args){
-        Object[] obj = new Object[]{"text", 8, new Rectangle(50,80), new Integer[]{2,3,4,5}, true, false, null};
+        Object[] obj = new Object[]{"text", 8, new Rectangle(50,80), new Integer[]{2,3,4,5},
+                new Double[]{3.5,8.1,7.2}, true, false, null};
         dumpArray(obj);
     }
 
