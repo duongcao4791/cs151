136f2c06f72437b45e39e6bbfa542f745baf9223
Duong Cao <duong@Duongs-MBP.hsd1.ca.comcast.net>
2019-11-17 17:14:03 -0800

    fix bug

--- hw4/question1b/src/BankAccount.java
+++ hw4/question1b/src/BankAccount.java
@@ -8,26 +8,22 @@ public class BankAccount {
         balance = 0;
     }
 
-    public double getBalance(){
-        return balance;
-    }
-
 
     public synchronized void deposit(double amount){
         System.out.print("Deposit " + amount);
         balance = balance + amount;
-        System.out.println(", New balance is " + balance);
+        System.out.println(". New balance is " + balance);
 
     }
 
     public synchronized void withdraw(double amount){
             System.out.print("Withdraw " + amount);
             if (balance<amount){
-            System.out.println(" Account is overdrawn, cannot withdraw!!!");
+            System.out.println(". Account is overdrawn, cannot withdraw!!!");
             }
             else {
                 balance = balance - amount;
-                System.out.println(", New balance is " + balance);
+                System.out.println(". New balance is " + balance);
         }
 
     }
